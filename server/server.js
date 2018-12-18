3b5833593aa723a975171aef867c1acdb7e1a408
magvin <zans.litinskis@gmail.com>
2018-12-18 18:41:24 +0000

    bug fix

--- server/server.js
+++ server/server.js
@@ -271,57 +271,70 @@ app.get('/api/users/removeimage', auth, admin, (req, res) => {
 
 
 //====CART =====///
-app.post('/api/users/addToCart', auth, (req, res) => {
+app.post('/api/users/successBuy', auth, (req, res) => {
+    let history = [];
+    let transactionData = {}
 
-    User.findOne({ _id: req.user._id }, (err, doc) => {
-
-        let duplicate = false;
-        doc.cart.forEach((item) => {
-
-            if (item.id == req.query.productId) {
-                duplicate = true;
-
-            }
-
+    // user history
+    req.body.cartDetail.forEach((item) => {
+        history.push({
+            dateOfPurchase: Date.now(),
+            name: item.name,
+            brand: item.brand.name,
+            id: item._id,
+            price: item.price,
+            quantity: item.quantity,
+            paymentId: req.body.paymentData.paymentID
         })
-        if (duplicate) {
-
-            User.findOneAndUpdate(
-
-                { _id: req.user._id, "cart.id": mongoose.Types.ObjectId(req.query.productId) },
-                { $inc: { "cart.$.quantity": 1 } },
-                { new: true },
-                () => {
-                    if (err) return res.json({ success: false, err });
-                    res.status(200).json(doc.cart)
-                }
-            )
-
-        } else {
-
-            User.findOneAndUpdate(
-
-                { _id: req.user._id },
-                {
-                    $push: {
-                        cart: {
-                            id: mongoose.Types.ObjectId(req.query.productId),
-                            quantity: 1,
-                            date: Date.now()
-                        }
-                    }
-                }, { new: true }, (err, doc) => {
-                    if (err) return res.json({ success: false, err });
-                    res.status(200).json(doc.cart)
-                }
-
-
-            )
-
-
-        }
-
     })
+
+    // PAYMENTS DASH
+    transactionData.user = {
+        id: req.user._id,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        email: req.user.email
+    }
+    transactionData.data = req.body.paymentData;
+    transactionData.product = history;
+
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $push: { history: history }, $set: { cart: [] } },
+        { new: true },
+        (err, user) => {
+            if (err) return res.json({ success: false, err });
+
+            const payment = new Payment(transactionData);
+            payment.save((err, doc) => {
+                if (err) return res.json({ success: false, err });
+                let products = [];
+                doc.product.forEach(item => {
+                    products.push({ id: item.id, quantity: item.quantity })
+                })
+
+                async.eachSeries(products, (item, callback) => {
+                    Product.update(
+                        { _id: item.id },
+                        {
+                            $inc: {
+                                "sold": item.quantity
+                            }
+                        },
+                        { new: false },
+                        callback
+                    )
+                }, (err) => {
+                    if (err) return res.json({ success: false, err })
+                    res.status(200).json({
+                        success: true,
+                        cart: user.cart,
+                        cartDetail: []
+                    })
+                })
+            });
+        }
+    )
 })
 
 
